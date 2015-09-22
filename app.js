9e15a4782a8e0de6133cddaba5eecd87fb40d061
Cole Willsea <coleww@gmail.com>
2015-09-22 06:38:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -14,38 +14,20 @@ window.setTimeout(function(){
 
 
 window.setTimeout(function(){
-  $('.window').css({opacity: 1});
-  $textDude.append("Suzanne Duchamp Ipsum Dolar Etsum Marcel Broken And Restored Loremiplication what what ok now");
-}, 2000);
-
-window.setTimeout(function(){
-  $('.star').css({opacity: 1});
-  $textDude.append("Suzanne Duchamp Ipsum Dolar Etsum Marcel Broken And Restored Loremiplication what what ok now");
-}, 3000);
-
-window.setTimeout(function(){
-  $('.tower').css({opacity: 1});
-  $textDude.append("Suzanne Duchamp Ipsum Dolar Etsum Marcel Broken And Restored Loremiplication what what ok now");
-}, 4000);
-
-window.setTimeout(function(){
     var $circles = $('.circle');
   window.setInterval(function(){
     var circle = _.sample($circles);
     $(circle).css({opacity: Math.random()});
   }, 300);
-  $textDude.append("Suzanne Duchamp Ipsum Dolar Etsum Marcel Broken And Restored Loremiplication what what ok now");
 }, 5000);
 
 window.setTimeout(function(){
   $('.text').css({opacity: 1});
-  $textDude.append("Suzanne Duchamp Ipsum Dolar Etsum Marcel Broken And Restored Loremiplication what what ok now");
 }, 6000);
 
 
 window.setTimeout(function(){
   clickyStage();
-  $textDude.append("Suzanne Duchamp Ipsum Dolar Etsum Marcel Broken And Restored Loremiplication what what ok now");
 }, 10000);
 function clickyStage(){
   var otherSaveAs = saveAs;
